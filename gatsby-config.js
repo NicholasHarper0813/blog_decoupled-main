3328214568d6befbb9ee3b72ef0505f9eafdf39b
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 18:11:47 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  siteMetadata: {
+  siteMetadata:
+  {
     title: `Decoupled.blog`,
     description: `Resources for your Decoupled WordPress journey.`,
     author: `@nicholas`,
@@ -12,21 +13,24 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-mdx`,
-      options: {
+      options: 
+      {
         defaultLayouts: { default: path.resolve('./src/components/layout.js') },
       },
     },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
+      options: 
+      {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
+      options: 
+      {
         name: `pages`,
         path: `${__dirname}/src/pages`
       }
@@ -35,13 +39,14 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
-      options: {
+      options: 
+      {
         name: `Decoupled.blog`,
         short_name: `Decoupled.blog`,
+        display: `minimal-ui`,
         start_url: `/`,
         background_color: `#205D86`,
         theme_color: `#205D86`,
-        display: `minimal-ui`,
         icon: `src/images/decoupled-blog-favicon.png`,
       },
     },
